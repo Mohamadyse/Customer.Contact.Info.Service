551a88904e3760ea49dbba41b9d96fbff5b4a999
Mohamad Eddin <Mohamad.Eddin@skandia.se>
2021-12-05 20:33:51 +0100

    Created CustomesDBContext and class library project for data layer

---
+++ XYZ.Customer.Contacts.Service.Data/Models/CustomersDBContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace XYZ.Customer.Contacts.Service.Data.Models
+{
+    public partial class CustomersDBContext : DbContext
+    {
+        public CustomersDBContext()
+        {
+        }
+
+        public CustomersDBContext(DbContextOptions<CustomersDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<CustomersDb> CustomersDb { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustomersDb>(entity =>
+            {
+                entity.HasKey(e => e.Prn)
+                    .HasName("PK__Customer__C5732E114DAC1815");
+
+                entity.ToTable("CustomersDB");
+
+                entity.Property(e => e.Prn)
+                    .HasColumnName("PRN")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("EMail")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
